826bc4efd6302690c40d5e51d375989b19fa35f5
cary wang <carywang@carys-MacBook.local>
2019-07-23 15:12:38 +0800

    wechat

--- flask_app/task/taskExec.py
+++ flask_app/task/taskExec.py
@@ -9,15 +9,15 @@ bp = Blueprint('task', __name__, url_prefix='/task')
 
 @bp.route('/drinkReminder',methods=['GET'])
 def drinkReminder():
-    toUser = 'ocYwz5oq8kZxYTVFoCocZ4Sq_zog'
-    templateId = 'lvB_ErsV-LFeRaJC56qfJbGcufxfWr5ArTlzdjfk0xA'
+    toUser = 'ocYwz5jS4kU8GuUZDM9KmYLsi99w'
+    templateId = '6iyPcqFBw7-gm6QSqj9zjyZeRl-zM2FheCnB4xWkK34'
     data = {
-        "fisrt" : {
+        "first" : {
             "value" : "Remember to Drink Your Water~",
             "color" : "#173177"
         },
-        "now" : {
-            "value" : datetime.now().strftime("%H:%M:%S"),
+        "task" : {
+            "value" : "8 cups of water a day makes all the pains away",
             "color" : "#173177"
         },
         "remark" : {
